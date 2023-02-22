4e81ad7369e9f9fd228e906f8dd97bdaddd419ba
AutoCL2023 <544426620@qq.com>
2023-02-22 08:43:41 +0100

    Create new Knowledge base with selected features from feature selection

--- AutoCL/examples/feature_selection.py
+++ AutoCL/examples/feature_selection.py
@@ -4,22 +4,25 @@ import os
 import time
 from random import shuffle
 import numpy as np
+from owlapy.owlready2 import OWLOntologyManager_Owlready2
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.feature_selection import SelectKBest, SelectFpr
 from sklearn.preprocessing import MultiLabelBinarizer
 from scipy import sparse as sp
 from sklearn.feature_selection import chi2, f_classif,  mutual_info_classif
-from owlready2 import get_ontology, default_world
+from owlready2 import get_ontology, default_world, destroy_entity
 import logging
 
 from examples.evolearner_feature_selection import EvoLearnerFeatureSelection
 from examples.search import calc_prediction
 from ontolearn.knowledge_base import KnowledgeBase
 from ontolearn.learning_problem import PosNegLPStandard
-from owlapy.model import OWLNamedIndividual, IRI
+from owlapy.model import OWLNamedIndividual, IRI, AddImport, OWLImportsDeclaration
+
+from AutoCL.ontolearn.concept_learner import EvoLearner
 
 LOG_FILE = 'featureSelection.log'
-DATASET = 'hepatitis'
+DATASET = 'carcinogenesis_lp'
 
 logging.basicConfig(filename=LOG_FILE,
                     filemode="a",
@@ -28,7 +31,7 @@ logging.basicConfig(filename=LOG_FILE,
                     level=logging.INFO)
 
 try:
-    os.chdir("Ontolearn/examples")
+    os.chdir("AutoCL/examples")
 except FileNotFoundError:
     logging.error(FileNotFoundError)
     pass
@@ -265,6 +268,16 @@ def transform_data_properties(onto, pos, neg):
         return df_transposed_categorical_dtype, df_transposed_numeric_dtype
 
 
+def create_new_kb(onto, features_object_properties, features_data_properties_categorical, feature_data_prop_numeric):
+    relevant_features = features_object_properties + features_data_properties_categorical + feature_data_prop_numeric
+    all_features = list(get_data_properties(onto)) + list(get_object_properties(onto))
+    data_prop_to_remove = [x for x in all_features if x.name not in relevant_features]
+    for prop in data_prop_to_remove:
+        destroy_entity(prop)
+    onto.save("newkb.owl")
+    return KnowledgeBase(path="./newkb.owl")
+
+
 if __name__ == "__main__":
     onto = get_ontology(settings['data_path']).load()
     kb = KnowledgeBase(path=settings['data_path'])
@@ -289,7 +302,7 @@ if __name__ == "__main__":
         object_properties_df = transform_object_properties(onto, p, n)
         bool_dtype_df, numeric_dtype_df = transform_data_properties(onto, p, n)
         feature_names = []
-        features_data_properties_categoical = []
+        features_data_properties_categorical = []
         feature_data_prop_numeric = []
         features_object_properties = []
 
@@ -299,23 +312,20 @@ if __name__ == "__main__":
             features_object_properties = list(set(map(lambda x: x.split("_feature_name_")[0], features_object_properties)))
 
         if not bool_dtype_df.empty:
-            features_data_properties_categoical = list(select_k_best_features(bool_dtype_df, chi2))
+            features_data_properties_categorical = list(select_k_best_features(bool_dtype_df, chi2))
 
         if not numeric_dtype_df.empty:
             feature_data_prop_numeric = list(select_k_best_features(numeric_dtype_df, mutual_info_classif))
 
         logging.info(f"OBJECT_PROP:{features_object_properties}")
         logging.info(f"DATA_PROP_Numeric:{feature_data_prop_numeric}")
-        logging.info(f"DATA_PROP_BOOL:{feature_data_prop_numeric}")
+        logging.info(f"DATA_PROP_BOOL:{features_data_properties_categorical}")
 
+        new_kb = create_new_kb(onto, features_object_properties, features_data_properties_categorical, feature_data_prop_numeric)
         st = time.time()
-        model = EvoLearnerFeatureSelection(knowledge_base=kb,
-                                           feature_obj_prop_name=features_object_properties,
-                                           feature_data_categorical_prop=features_data_properties_categoical,
-                                           feature_data_numeric_prop=feature_data_prop_numeric)
-
+        model = EvoLearner(knowledge_base=new_kb)
         model.fit(lp)
-        model.save_best_hypothesis(n=3, path='Predictions_{0}'.format(str_target_concept))
+        model.save_best_hypothesis(n=3, path=f'Predictions_{str_target_concept}')
         hypotheses = list(model.best_hypotheses(n=1))
         predictions = model.predict(individuals=list(test_pos | test_neg),
                                     hypotheses=hypotheses)
